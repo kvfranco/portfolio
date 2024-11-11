d0e35f529c4ab105b818a727a5c2fe7d645c1cd0
Kevin Franco <42453809+kvfranco@users.noreply.github.com>
2024-11-11 04:23:07 -0300

    update portfolio

---
+++ script/script.js
@@ -0,0 +1,18 @@
+function trocarCorDeFundo() {
+  const background = document.querySelector('body');
+  const iconeTrocarFundo = document.querySelector('#icone-trocar-fundo');
+  const iconeSite = document.querySelector('#icone-site');
+  if (background.className === 'modo-escuro'){
+    background.style.backgroundColor = '#ffffff';
+    background.style.color = '#213547';
+    background.className = 'modo-claro';
+    iconeTrocarFundo.src = './assets/icones/light-mode-icon.svg';
+    iconeSite.src = './assets/icones/letra-k-escura.svg';
+    return;
+  }
+  iconeTrocarFundo.src = './assets/icones/dark-mode-icon.svg';
+  iconeSite.src = './assets/icones/letra-k-claro.svg'
+  background.style.backgroundColor = '#213547';
+  background.style.color = '#ffffff';
+  background.className = 'modo-escuro';
+}
